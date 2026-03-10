17a7dd4a030a2dc4310dc80f79083bcb3e598078
tamil12345-spec <tamilarasi3086@gmail.com>
2026-03-10 11:14:09 +0530

    fe-re-depoly

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://depoly-backend-qyda.onrender.com//api',
+  baseURL: 'https://depoly-backend-qyda.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
   timeout: 10000,
 });
